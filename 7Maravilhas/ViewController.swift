3df985018268c75000796aba64477cc2b69a3533
athena <athena@ciandt.com>
2021-07-19 15:45:40 -0300

    create project

---
+++ 7Maravilhas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7Maravilhas
+//
+//  Created by Athena Fernandes Sarantôpoulos on 19/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
